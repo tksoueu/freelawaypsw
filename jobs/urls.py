39c0042a25593660b1cb19939a44e37e601737b0
Tailk Jardim <tkjardim@gmail.com>
2022-04-13 01:34:34 -0300

    1.4
    added modals and aceitar job

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    path('encontrar_jobs/', views.encontrar_jobs, name="encontrar_jobs")
-    
+    path('encontrar_jobs/', views.encontrar_jobs, name="encontrar_jobs"),
+    path('aceitar_job/<int:id>/', views.aceitar_job, name="aceitar_job"),
+    path('perfil/', views.perfil, name="perfil")
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Jobs
 from datetime import datetime
 
@@ -41,3 +41,15 @@ def encontrar_jobs(request):
         else:
             jobs = Jobs.objects.filter(reservado=False)
         return render(request, 'encontrar_jobs.html', {'jobs': jobs})
+
+def aceitar_job(request, id):
+    job = Jobs.objects.get(id=id)
+    job.profissional = request.user
+    job.reservado = True
+    job.save()
+
+    return redirect('/jobs/encontrar_jobs')
+
+def perfil(request):
+    if request.method == "GET":
+        return render(request, 'perfil.html')
