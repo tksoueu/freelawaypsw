bd29228b75aca12b45c36e2bbf4f5f628ea04f7c
Tailk Jardim <tkjardim@gmail.com>
2022-04-18 01:04:53 -0300

    1.7

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a&iz7@jxdi%+!y_j58!%#nw3pke6zp$1oh_=%_a4fch-f@o^#9'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
